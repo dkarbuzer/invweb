480a088a63c0edda1d2f5c563693fc036d20d89d
dkarbuzer <dkarbuzer2203@gmail.com>
2024-09-20 04:43:07 +0300

    updt

--- scripts/lucky-jet.js
+++ scripts/lucky-jet.js
@@ -6,24 +6,38 @@ const luckyJetDisplayBg = document.getElementById('luckyJetDisplayBg');
 const clickSoundEffect = document.getElementById('clickSoundEffect');
 clickSoundEffect.volume = 0.2;
 
-
-//Функция для генерации случайного числа
+// Функция для генерации случайного числа
 function getRandomFloat(min, max, decimals) {
-    const str = (Math.random() * (max - min) + min).toFixed(
-      decimals,
-    );
+    const str = (Math.random() * (max - min) + min).toFixed(decimals);
     return parseFloat(str);
-  }
+}
 
+// Таймер на 20 секунд
+function startCooldown(seconds) {
+    let remainingTime = seconds;
+    
+    buttonGetSignal.disabled = true;
+    const interval = setInterval(() => {
+        remainingTime--;
+        buttonGetSignal.innerText = language === "ru" ? `Подождите ${remainingTime} сек` : `Wait ${remainingTime} sec`;
+
+        if (remainingTime <= 0) {
+            clearInterval(interval);
+            buttonGetSignal.innerText = language === "ru" ? "получить сигнал" : "get signal";
+            buttonGetSignal.disabled = false;
+        }
+    }, 1000);
+}
 
 buttonGetSignal.onclick = function() {
     clickSoundEffect.play();
 
     let receivingSignal = getRandomFloat(1.20, 3.99, 2);
 
-    if ((receivingSignal.toString().length == 3)) {
+    if (receivingSignal.toString().length == 3) {
         receivingSignal += "0";
-    } if ((receivingSignal.toString().length == 1)) {
+    }
+    if (receivingSignal.toString().length == 1) {
         receivingSignal += ".00";
     }
     signalText.innerHTML = `${receivingSignal}${"x"}`;
@@ -37,8 +51,11 @@ buttonGetSignal.onclick = function() {
 
     setTimeout(() => {
         buttonGetSignal.classList.remove("hidden");
-        buttonGetSignal.disabled = false;
         luckyJetDisplayBg.style.animation = "none";
         luckyJetDisplayBg.style.opacity = 1;
+
+        // Запускаем таймер на 20 секунд
+        startCooldown(20);
     }, reloadDuration);
 }
+
